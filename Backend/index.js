4ae5b2c6fc93bfb51f4f8c364244aae34abce699
Akshar <aksharansh123@gmail.com>
2023-07-24 03:39:43 +0530

    update

---
+++ Backend/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const body = require("body-parser");
+const path = require("path");
+const app = express();
+const collection = require("./mongodb"); // Assuming mongodb.js is in the same Backend folder
+
+const port = 3000;
+
+app.use(body.urlencoded({ extended: true }));
+
+// Serve static files from the "Frontend" folder
+app.use(express.static(path.join(__dirname, "../Frontend")));
+
+// GET route for "/"
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../Frontend/index.html"));
+});
+
+// POST route for login
+app.post("/", async function (req, res) {
+  var first = req.body.Firstname;
+  var second = req.body.Secondname;
+  var email = req.body.email;
+
+  try {
+    // Check if the user exists in the database
+    const user = await collection.findOne({
+      Email: email,
+      First_name: first,
+      Last_name: second,
+    });
+
+    if (user) {
+      // User exists, authentication is successful
+      authenticatedUser = email;
+      res.redirect("/home");
+    } else {
+      // User does not exist, authentication fails
+      res.status(401).sendFile(path.join(__dirname, "../Frontend/failure.html"));
+    }
+  } catch (error) {
+    console.error("Error authenticating user:", error);
+    res.status(500).send("An error occurred while authenticating the user");
+  }
+});
+
+// GET route for "home"
+app.get("/home", function (req, res) {
+  res.sendFile(path.join(__dirname, "../Frontend/home.html"));
+});
+
+app.post("/home", function (req, res) {
+  res.redirect("/Alphabet");
+});
+
+// Add other routes for your app here
+// For example, to serve "/Alphabet" and "/poem"
+//Get Route for sign up
+// GET route for the signup page
+app.get("/signup", function (req, res) {
+  res.sendFile(path.join(__dirname, "../Frontend/signup.html"));
+});
+
+// POST route for sign up
+
+app.post("/signup", async function (req, res) {
+  // ...
+
+  try {
+    // Save the user's information to the database
+    const user = await collection.create({
+      First_name: req.body.Firstname,
+      Last_name: req.body.Secondname,
+      Email: req.body.email,
+    });
+
+    // Set a unique identifier for the user (e.g., user ID)
+    const userId = user._id;
+
+    // Send the success message to the user
+    res.send("User registered successfully! Please proceed to the login page.");
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).send("An error occurred while registering the user.");
+  }
+});
+
+// MONGO KA CODE
+
+app.listen(port, function (err) {
+  if (err) {
+    console.error("Error starting the server:", err);
+  } else {
+    console.log(`Server is running on port ${port}`);
+  }
+});
--- Frontend/script.js
+++ Frontend/script.js
@@ -18,136 +18,136 @@
 
     switch (key) {
       case "A":
-        var A = new Audio("../public/A.mpeg");
+        var A = new Audio("Sounds/A.mpeg");
         A.play();
         break;
 
       case "B":
-        var B = new Audio("../public/B.mpeg");
+        var B = new Audio("Sounds/B.mpeg");
         B.play();
         break;
 
       case "C":
-        var C = new Audio("../public/C.mpeg");
+        var C = new Audio("Sounds/C.mpeg");
         C.play();
         break;
 
       case "D":
-        var D = new Audio("../public/D.mpeg");
+        var D = new Audio("Sounds/D.mpeg");
         D.play();
         break;
 
       case "E":
-        var E = new Audio("../public/E.mpeg");
+        var E = new Audio("Sounds/E.mpeg");
         E.play();
         break;
 
       case "F":
-        var F = new Audio("../public/F.mpeg");
+        var F = new Audio("Sounds/F.mpeg");
         F.play();
         break;
 
       case "G":
-        var G = new Audio("../public/G.mpeg");
+        var G = new Audio("Sounds/G.mpeg");
         G.play();
         break;
 
 
        case "H":
-        var H = new Audio("../public/H.mpeg");
+        var H = new Audio("Sounds/H.mpeg");
         H.play();
         break;
 
       case "I":
-        var I = new Audio("../public/I.mpeg");
+        var I = new Audio("Sounds/I.mpeg");
         I.play();
         break;
 
       case "J":
-        var J = new Audio("../public/J.mpeg");
+        var J = new Audio("Sounds/J.mpeg");
         J.play();
         break;
 
       case "K":
-        var K = new Audio("../public/K.mpeg");
+        var K = new Audio("Sounds/K.mpeg");
         K.play();
         break;
 
       case "L":
-        var L = new Audio("../public/L.mpeg");
+        var L = new Audio("Sounds/L.mpeg");
         L.play();
         break;
 
       case "M":
-        var M = new Audio("../public/M.mpeg");
+        var M = new Audio("Sounds/M.mpeg");
         M.play();
         break;
 
       case "N":
-        var N = new Audio("../public/N.mpeg");
+        var N = new Audio("Sounds/N.mpeg");
         N.play();
         break;
 
 
 
        case "O":
-        var O = new Audio("../public/O.mpeg");
+        var O = new Audio("Sounds/O.mpeg");
         O.play();
         break;
 
       case "P":
-        var P = new Audio("../public/P.mpeg");
+        var P = new Audio("Sounds/P.mpeg");
         P.play();
         break;
 
       case "Q":
-        var Q = new Audio("../public/Q.mpeg");
+        var Q = new Audio("Sounds/Q.mpeg");
         Q.play();
         break;
 
       case "R":
-        var R = new Audio("../public/R.mpeg");
+        var R = new Audio("Sounds/R.mpeg");
         R.play();
         break;
 
       case "S":
-        var S = new Audio("../public/S.mpeg");
+        var S = new Audio("Sounds/S.mpeg");
         S.play();
         break;
 
       case "T":
-        var T = new Audio("../public/T.mpeg");
+        var T = new Audio("Sounds/T.mpeg");
         T.play();
         break;
 
       case "U":
-        var U= new Audio("../public/U.mpeg");
+        var U= new Audio("Sounds/U.mpeg");
         U.play();
         break;
 
 
        case "V":
-        var V = new Audio("../public/V.mpeg");
+        var V = new Audio("Sounds/V.mpeg");
         V.play();
         break;
 
       case "W":
-        var W = new Audio("../public/W.mpeg");
+        var W = new Audio("Sounds/W.mpeg");
         W.play();
         break;
 
       case "X":
-        var X = new Audio("../public/X.mpeg");
+        var X = new Audio("Sounds/X.mpeg");
         X.play();
         break;
 
       case "Y":
-        var Y = new Audio("../public/Y.mpeg");
+        var Y = new Audio("Sounds/Y.mpeg");
         Y.play();
         break;
 
       case "Z":
-        var Z = new Audio("../public/Z.mpeg");
+        var Z = new Audio("Sounds/Z.mpeg");
         Z.play();
         break;
 
